ddd862da797132785b4f09ed7c2ccf4d6e42c15a
vitto2 <vitor2071@gmail.com>
2022-11-07 17:25:55 -0300

    tornando a column principal um singleChieldScrollView para resolver problema de overflow ao abrir o teclado

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -7,9 +7,12 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: const Login(),
     );
   }
 }
--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -17,53 +17,55 @@ class _LoginState extends State<Login> {
       backgroundColor: const Color.fromARGB(255, 51, 53, 66),
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 80,
-            ),
-            MyTextWidget(
-              "Welcome to Locaticus",
-              Colors.white,
-              FontWeight.w400,
-            ),
-            const Divider(
-              indent: 150,
-              endIndent: 150,
-              color: Colors.white,
-            ),
-            SizedBox(
-              width: 150,
-              child: Image.asset('assets/images/map.png'),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            SizedBox(
-              width: 350,
-              child: MyTextForm(
-                hintText: "E-mail Address",
-                prefixIcon: const Icon(Icons.mail),
-                keyboardType: TextInputType.emailAddress,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 80,
               ),
-            ),
-            SizedBox(
-              width: 350,
-              child: MyTextForm(
-                hintText: "• • • • • •",
-                prefixIcon: const Icon(Icons.lock),
-                keyboardType: TextInputType.visiblePassword,
+              MyTextWidget(
+                "Welcome to Locaticus",
+                Colors.white,
+                FontWeight.w400,
               ),
-            ),
-            SizedBox(
-              width: 350,
-              height: 70,
-              child: MyButton(
-                backgroundColor: const Color.fromARGB(255, 67, 182, 126),
+              const Divider(
+                indent: 150,
+                endIndent: 150,
+                color: Colors.white,
               ),
-            ),
-          ],
+              SizedBox(
+                width: 150,
+                child: Image.asset('assets/images/map.png'),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: 350,
+                child: MyTextForm(
+                  hintText: "E-mail Address",
+                  prefixIcon: const Icon(Icons.mail),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+              ),
+              SizedBox(
+                width: 350,
+                child: MyTextForm(
+                  hintText: "• • • • • •",
+                  prefixIcon: const Icon(Icons.lock),
+                  keyboardType: TextInputType.visiblePassword,
+                ),
+              ),
+              SizedBox(
+                width: 345,
+                height: 70,
+                child: MyButton(
+                  backgroundColor: const Color.fromARGB(255, 67, 182, 126),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
